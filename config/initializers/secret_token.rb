b0141e141e2ff2595e4858ceef3187d67e8df552
Ryan Fitzgerald <ryan@gymlogr.com>
2010-10-19 00:58:02 -0400

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SleepingGiant::Application.config.secret_token = '60d045505021c44c1ce85926bb3365ae856696cfc5eb093d728fdce78e23a4764e4f6d519046f36ccbe55a25c1761aaab0f538b10c55d52151447c02fe02722e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SleepingGiant::Application.config.session_store :cookie_store, :key => '_sleeping_giant_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# SleepingGiant::Application.config.session_store :active_record_store
